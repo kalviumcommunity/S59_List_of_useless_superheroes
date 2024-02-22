483b9d7d313384e29188668f01395ea5e8df8767
Merlinkk <anshulkashyap2504@gmail.com>
2024-02-22 13:24:17 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const Express=require("express");
-const app=Express();
+const express=require("express");
+const app=express();
 
 const port=5000;
 
 app.get("/",(req,res)=>{
-    res.send("<h1>h</h1>")
+    res.send("<h1>Welcome to the curated collection of superheroes who, in certain timelines or specific events, demonstrated a level of impracticality or perceived uselessness. The superheroes included in this list may have faced challenges where their contributions were notably limited, showcasing the diversity of their experiences within the chosen universe—be it Marvel or DC.</h1>")
 });
 
 app.listen(port,()=>{
